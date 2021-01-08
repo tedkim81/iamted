dff6dbd77cea9038039dace3e187bd36c1daf64b
tedkim81 <tedkim.developer@gmail.com>
2021-01-08 11:19:16 +0900

    환경설정 분리

--- iamted/settings.py
+++ iamted/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_@a9xjwq7dq(dw_(hwau4#78t88-h6fkscwxrhe)sm$^rxec9g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = eval(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = ['*']
 
