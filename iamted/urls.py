9633bc9c3c89269015f620b46bc92a19443622d8
tedkim81 <tedkim.developer@gmail.com>
2021-01-10 10:20:04 +0900

    debug mode 변경

--- iamted/urls.py
+++ iamted/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^goal_modal/(?P<pk>\d+)/$', views.GoalModalView.as_view(), name='goal-modal'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
+if settings.DEBUG_TOOLBAR:
     import debug_toolbar
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
