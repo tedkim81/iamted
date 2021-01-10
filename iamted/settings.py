1d63ee24aaaaf9e972acfe12a200a6fab07a973b
tedkim81 <tedkim.developer@gmail.com>
2021-01-10 10:11:08 +0900

    debug mode 변경

--- iamted/settings.py
+++ iamted/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = eval(os.environ.get('DEBUG'))
 DEBUG_TOOLBAR = eval(os.environ.get('DEBUG_TOOLBAR'))
 
 ALLOWED_HOSTS = ['*']
