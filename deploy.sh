c32f9afecc974f2cd096fdb81a36930ff2b4ea77
Ted Kim <tedkim.developer@gmail.com>
2026-01-18 01:42:54 +0900

    privacy-policy.html 문서가 다른 곳에 있는 상태에서 배포했다가 문서가 사라지는 문제 발생. 문서를 여기에 추가하고 배포 코드 수정

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ echo "🚀 Deploying to S3 (iamted.kim)..."
 # S3에 파일 동기화
 aws s3 sync ./static/html/ s3://iamted.kim/ \
   --profile personal \
-  --acl public-read \
   --cache-control "max-age=300" \
   --exclude ".*" \
   --delete
@@ -17,4 +16,3 @@ else
     echo "❌ Deployment failed!"
     exit 1
 fi
-
